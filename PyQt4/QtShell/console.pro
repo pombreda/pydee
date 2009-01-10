2729d04b3fced446ae5fcedf57cec80a6cfd51bf
Pierre Raybaut <pr@pythonxy.com>
2009-01-10 18:58:14 +0100

    French QtLinguist translation files

---
+++ PyQt4/QtShell/console.pro
@@ -0,0 +1,9 @@
+SOURCES	     += console.py
+SOURCES	     += qsciwidgets.py
+SOURCES	     += qtwidgets.py
+SOURCES	     += shell.py
+SOURCES	     += widgets.py
+SOURCES	     += config.py
+SOURCES	     += arrayeditor.py
+TRANSLATIONS += console_fr.ts
+
---
+++ PyQt4/QtShell/console_fr.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="fr">
+<context>
+    <name>ArrayEditor</name>
+    <message>
+        <location filename="arrayeditor.py" line="132"/>
+        <source>Array editor</source>
+        <translation>Éditeur de tableaux</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="187"/>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="151"/>
+        <source>Resize</source>
+        <translation>Adapter</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="154"/>
+        <source>Background color</source>
+        <translation>Couleur d&apos;arrière-plan</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="182"/>
+        <source>Float formatting</source>
+        <translation>Format de flottant</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="195"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="arrayeditor.py" line="196"/>
+        <source>Format (%s) is incorrect</source>
+        <translation>Le format (%s) n&apos;est pas valide</translation>
+    </message>
+</context>
+<context>
+    <name>ConsoleWindow</name>
+    <message>
+        <location filename="console.py" line="91"/>
+        <source>PyQtShell Console</source>
+        <translation>Console PyQtShell</translation>
+    </message>
+    <message>
+        <location filename="console.py" line="96"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="console.py" line="97"/>
+        <source>Are you sure you want to quit?</source>
+        <translation>Souhaitez-vous réellement quitter ?</translation>
+    </message>
+    <message>
+        <location filename="console.py" line="65"/>
+        <source>Welcome to PyQtShell demo!</source>
+        <translation>Bienvenue dans la console de démontration de PyQtShell !</translation>
+    </message>
+</context>
+<context>
+    <name>CurrentDirChanger</name>
+    <message>
+        <location filename="widgets.py" line="121"/>
+        <source>Current directory</source>
+        <translation>Répertoire courant</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="126"/>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="140"/>
+        <source>Select directory</source>
+        <translation>Sélectionner un répertoire</translation>
+    </message>
+</context>
+<context>
+    <name>QShell</name>
+    <message>
+        <location filename="widgets.py" line="60"/>
+        <source>&amp;Run...</source>
+        <translation>Exécute&amp;r...</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="61"/>
+        <source>Ctrl+R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="62"/>
+        <source>Run a Python script</source>
+        <translation>Exécuter un script Python</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="63"/>
+        <source>&amp;Font...</source>
+        <translation>&amp;Police...</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="65"/>
+        <source>Set shell font style</source>
+        <translation>Changer la police d&apos;écriture de la console</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="66"/>
+        <source>History...</source>
+        <translation>Historique...</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="68"/>
+        <source>Set history max entries</source>
+        <translation>Modifier le nombre d&apos;entrées maximum de l&apos;historique</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="75"/>
+        <source>&amp;Console</source>
+        <translation>&amp;Console</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="81"/>
+        <source>Run Python script</source>
+        <translation>Exécuter un script Python</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="82"/>
+        <source>Python scripts</source>
+        <translation>Scripts Python</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="94"/>
+        <source>Select a new font</source>
+        <translation>Sélectionner une police d&apos;écriture</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="105"/>
+        <source>History</source>
+        <translation>Historique</translation>
+    </message>
+    <message>
+        <location filename="widgets.py" line="106"/>
+        <source>Maximum entries</source>
+        <translation>Nombre maximum d&apos;entrées</translation>
+    </message>
+</context>
+<context>
+    <name>QSimpleShell</name>
+    <message>
+        <location filename="qtwidgets.py" line="87"/>
+        <source>Please install QScintilla to enable autocompletion</source>
+        <translation>Merci d&apos;installer QScintilla pour activer la complétion de code</translation>
+    </message>
+    <message>
+        <location filename="qtwidgets.py" line="86"/>
+        <source>Type &quot;copyright&quot;, &quot;credits&quot; or &quot;license&quot; for more information.</source>
+        <translation>Entrer &quot;copyright&quot;, &quot;credits&quot; ou &quot;license&quot; pour des informations supplémentaires.</translation>
+    </message>
+    <message>
+        <location filename="qtwidgets.py" line="161"/>
+        <source>Busy...</source>
+        <translation>Occupé...</translation>
+    </message>
+</context>
+<context>
+    <name>QsciShell</name>
+    <message>
+        <location filename="qsciwidgets.py" line="63"/>
+        <source>Type &quot;object?&quot; for details on &quot;object&quot;</source>
+        <translation>Entrer &quot;objet?&quot; pour afficher des informations sur &quot;objet&quot;</translation>
+    </message>
+    <message>
+        <location filename="qsciwidgets.py" line="62"/>
+        <source>Type &quot;copyright&quot;, &quot;credits&quot; or &quot;license&quot; for more information.</source>
+        <translation>Entrer &quot;copyright&quot;, &quot;credits&quot; ou &quot;license&quot; pour des informations supplémentaires.</translation>
+    </message>
+    <message>
+        <location filename="qsciwidgets.py" line="174"/>
+        <source>Busy...</source>
+        <translation>Occupé...</translation>
+    </message>
+</context>
+<context>
+    <name>Shell</name>
+    <message>
+        <location filename="shell.py" line="54"/>
+        <source>Type &quot;copyright&quot;, &quot;credits&quot; or &quot;license&quot; for more information.</source>
+        <translation type="obsolete">Entrer &quot;copyright&quot;, &quot;credits&quot; ou &quot;license&quot; pour plus d&apos;informations.</translation>
+    </message>
+</context>
+</TS>
