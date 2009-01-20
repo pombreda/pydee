28c9d9945afc78af4a84bf4e02f01d2339fa9d93
Pierre Raybaut <pr@pythonxy.com>
2009-01-21 00:48:41 +0100

    0.1.0 PyQt4.QtShell --> PyQtShell

--- PyQtShell/console.py
+++ PyQtShell/console.py
@@ -3,7 +3,7 @@
 PyQtShell Console
 """
 
-__version__ = '0.0.11'
+__version__ = '0.1.0'
 
 import sys, os, platform
 from PyQt4.QtGui import QApplication, QMainWindow
--- qons
+++ qons
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from PyQt4.QtShell import console
+from PyQtShell import console
 console.main()
