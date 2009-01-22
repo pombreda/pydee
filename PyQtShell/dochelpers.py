1f73e5e09ae0e4ec51d47b784d9417ba98841087
Pierre Raybaut <pierre.raybaut@cea.fr>
2009-01-22 18:24:54 +0100

    Bugfix

--- PyQtShell/dochelpers.py
+++ PyQtShell/dochelpers.py
@@ -19,6 +19,7 @@ def getsource(obj):
 
 def getargtxt(obj, one_arg_per_line=True):
     """Get the names and default values of a function's arguments"""
+    sep = ', '
     if inspect.isfunction(obj) or inspect.isbuiltin(obj):
         func_obj = obj
     elif inspect.ismethod(obj):
@@ -27,17 +28,25 @@ def getargtxt(obj, one_arg_per_line=True):
         func_obj = getattr(obj, '__init__')
     else:
         return None
+    if not hasattr(func_obj, 'func_code'):
+        # Builtins are rejected here...
+        return None
     args, _, _ = inspect.getargs(func_obj.func_code)
     defaults = func_obj.func_defaults
-    for index, default in enumerate(defaults):
-        args[index+len(args)-len(defaults)] += '='+str(default)
-    textlist = ['']
+    if defaults is not None:
+        for index, default in enumerate(defaults):
+            args[index+len(args)-len(defaults)] += '='+str(default)
+    textlist = None
     for i_arg, arg in enumerate(args):
+        if textlist is None:
+            textlist = ['']
         textlist[-1] += arg
         if i_arg < len(args)-1:
-            textlist[-1] += ', '
+            textlist[-1] += sep
             if len(textlist[-1])>=32 or one_arg_per_line:
                 textlist.append('')
+    if inspect.isclass(obj):
+        textlist.remove('self'+sep)
     return textlist
     
 #import subprocess
