0621a53148101beebca200bbb30c3503227c4922
Pierre Raybaut <pierre.raybaut@cea.fr>
2009-02-17 18:20:31 +0100

    Bugfix / Editor: filenames/directories errors

--- PyQtShell/config.py
+++ PyQtShell/config.py
@@ -138,9 +138,9 @@ DEFAULTS = [
             ]
 
 DEV = osp.isfile(osp.join(osp.join(APP_PATH, osp.pardir), 'setup.py'))
-#DEV = False
+DEV = False
 from userconfig import UserConfig
-CONF = UserConfig('PyQtShell', DEFAULTS, version='0.0.18', load=(not DEV))
+CONF = UserConfig('PyQtShell', DEFAULTS, version='0.0.19', load=(not DEV))
 
 def get_conf_path(filename):
     """
