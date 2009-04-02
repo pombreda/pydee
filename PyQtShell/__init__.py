9ba10b6d2a7dc24cf6e2758d58feaff189b5b536
Pierre Raybaut <pierre.raybaut@cea.fr>
2009-04-02 14:28:01 +0200

    Matplotlib integration / Bugfix: "Save figure" was broken

--- PyQtShell/__init__.py
+++ PyQtShell/__init__.py
@@ -22,4 +22,4 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-__version__ = '0.3.17'
+__version__ = '0.3.18'
