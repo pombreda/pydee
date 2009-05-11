ef0195a3b2c657297c1647a85594ab6ce1115b81
Pierre Raybaut <pr@pythonxy.com>
2009-05-11 12:11:00 +0200

    todos

--- PyQtShell/__init__.py
+++ PyQtShell/__init__.py
@@ -22,5 +22,5 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-__version__ = '0.3.26'
+__version__ = '0.3.27'
 #TODO: Rename PyQtShell into pydee?
