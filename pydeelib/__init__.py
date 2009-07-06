2d0d80e27298dafd00fce74d6b22847eb7a13795
Pierre Raybaut <pierre.raybaut@gmail.com>
2009-07-06 23:11:07 +0200

    Pydee bugfix: attempt to fix Issue 23 (Can't import submodules in interactive console)

--- pydeelib/__init__.py
+++ pydeelib/__init__.py
@@ -27,7 +27,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '0.4.20'
+__version__ = '0.4.21'
 __license__ = __doc__
 
 def check_required_package(package, actual_str, required_str):
